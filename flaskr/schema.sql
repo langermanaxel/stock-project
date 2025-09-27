db4839d32507bc08280d697c932e33e01549d2b8
Axel Langerman <axellangerman@yahoo.com>
2025-09-27 20:14:16 -0300

    Login/Register in progress..

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -2,6 +2,10 @@ DROP TABLE IF EXISTS user;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
+  firstname TEXT NOT NULL,
+  lastname TEXT NOT NULL,
+  email TEXT NOT NULL UNIQUE COLLATE NOCASE,
   username TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
+  password_hash TEXT NOT NULL,
+  created_at TEXT NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ','now'))
 );
