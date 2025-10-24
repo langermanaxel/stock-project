96f17eefeacaa52f6e42eddd5764df269bf0e33d
Axel Langerman <axellangerman@yahoo.com>
2025-10-23 23:06:04 -0300

    Working on deployment...

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-from flaskr import create_app
+import os
+from flask import Flask
+from flaskr import create_app  # o la función donde creas tu app
 
 app = create_app()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # ✅ usa el puerto que Render define
+    app.run(host="0.0.0.0", port=port)
